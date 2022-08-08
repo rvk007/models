dbe554ff9531b562829166bcce0d3aaa6e83d29c
Rakhee Rakhee <rrakhee@nvidia.com>
2022-08-08 15:46:31 -0400

    Remove mulitple imports and update item feature names

--- examples/usecases/recsys22-examples/utils.py
+++ examples/usecases/recsys22-examples/utils.py
@@ -14,7 +14,8 @@ def process_item_features(DATA_FOLDER, category_coverage_min=0.8):
     df = df[df.feature_category_id.isin(categories_to_keep)]
     df = df[~df.feature_category_id.isin([[30, 4, 46, 28, 53, 1]])]
     df = df.pivot_table('feature_value_id', ['item_id'], 'feature_category_id').reset_index()
-    df.columns = [str(col) for col in df.columns]
+    df.columns = [str(col)+'_f' if isinstance(col, int) else str(col) for col in df.columns]
+#     df.columns = [str(col) for col in df.columns]
     return df
 
 # add timestamp and day features
@@ -60,5 +61,8 @@ def get_drepessi_recsys2022_dataset(input_path):
     
     purchases = purchases[['session_id','purchase_id']]
     sessions = cudf.merge(sessions, purchases, on='session_id', how='left')
-
-    return train, valid, item_features, sessions
+    
+    train = train.fillna(-1)
+    valid = valid.fillna(-1)
+    sessions = sessions.fillna(-1)
+    return train, valid, sessions
